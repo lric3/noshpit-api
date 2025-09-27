0efaace5a37d6b6c93e3e2021806fd058cfe6fda
lric3 <lrice012@icloud.com>
2025-09-27 07:38:47 -0500

    Initial commit

---
+++ src/main/java/com/lric3/noshpit_api/NoshpitApiApplication.java
@@ -0,0 +1,13 @@
+package com.lric3.noshpit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoshpitApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(NoshpitApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lric3/noshpit_api/NoshpitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lric3.noshpit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoshpitApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
